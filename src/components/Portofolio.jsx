6709402111c6ebd5b107dc7e689f3a2e733c5168
muhammad.fikran <muhammadfikran@Muhammads-MacBook-Air-2.local>
2023-09-24 11:41:20 +0700

    remove unecesarry padding

--- src/components/Portofolio.jsx
+++ src/components/Portofolio.jsx
@@ -50,7 +50,7 @@ const Portofolio = () => {
   return (
     <div
       name="portofolio"
-      className=" md:h-screen bg-gradient-to-b pt-40 from-gray-800 to-black w-full pt"
+      className=" md:h-screen bg-gradient-to-b pt-40 from-gray-800 to-black w-full "
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
